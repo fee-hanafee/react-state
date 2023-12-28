870c5ad22e367efd017080660793b810df08982b
fee-hanafee <hanafee.pass@gmail.com>
2023-12-28 10:14:55 +0700

    feat:add todo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 
 function App() {
   const [todos, setTodos] = React.useState(["HW", "Hangout"]);
+  const [newTodo, setNewtodo] = React.useState("");
 
+  const todoChange = (event) => {
+    setNewtodo(event.target.value);
+  };
+  const todoAddnew = () => {
+    setTodos((cur) => [...cur, newTodo]);
+    setNewtodo('')
+  };
   const todoRender = todos.map((todo, index) => <li key={index}>{todo}</li>);
   return (
     <div>
       <h1>My Todo</h1>
       <ul>
+        <input onChange={todoChange} value={newTodo}/>
+        <button onClick={todoAddnew}>add</button>
         {todoRender}
       </ul>
     </div>
