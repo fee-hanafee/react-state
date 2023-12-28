53ddb45c4a529aa3976c012488ddab16b11da714
fee-hanafee <hanafee.pass@gmail.com>
2023-12-28 09:49:11 +0700

    setup state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
 
 function App() {
+  const [todos, setTodos] = React.useState(["HW", "Hangout"]);
+
+  const todoRender = todos.map((todo, index) => <li key={index}>{todo}</li>);
   return (
-    <div >
-      <h1>Hello React</h1>
+    <div>
+      <h1>My Todo</h1>
+      <ul>
+        {todoRender}
+      </ul>
     </div>
   );
 }
